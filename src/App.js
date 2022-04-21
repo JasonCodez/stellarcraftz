3149ca7e4c398b82aede5c53ccdab8e063fd7b2b
JasonCodez <jason13garrett@gmail.com>
2022-04-20 20:34:30 -0500

    update site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Navbar from './components/Navbar';
+import Card from "./components/Card";
+import data from "./itemData";
 import './App.css';
 
 function App() {
+  const items = data.map(item => {
+     return <Card 
+               key={item.id}
+               img={item.img}
+               name={item.name}
+               price={item.price}
+            />
+  })
   return (
     <div className="App">
       <Navbar />
+      <div className='App-container'>
+         {items}
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import "../styles/Card.css"
+
+const Card = (props) => {
+   return (
+      <div className="Card">
+         <div className="Card-container">
+            <img className="Card-image" src={props.img} alt="item image"/>
+            <div className="Card-info">
+               <h3>{props.name}</h3>
+               <p>${props.price}</p>
+            </div>
+         </div>
+         
+      </div>
+   )
+}
+
+export default Card;
